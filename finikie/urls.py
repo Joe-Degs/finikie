cd2ab4d84bbd4c1beb93675daefd6ffd0ed092ca
Joseph Attah <jpedegs8990@gmail.com>
2022-05-13 17:45:08 +0000

    jkfdkf

--- finikie/urls.py
+++ finikie/urls.py
@@ -17,17 +17,9 @@ from re import A
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from rest_framework import viewsets
-from rest_framework import routers
-
-from finikie.quickstart import views
-
-router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(router.urls)),
+    path('api/', include('quickstart.urls', namespace='api')),
     path('api/', include('todo.urls', namespace='api')),
 ]
--- quickstart/views.py
+++ quickstart/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
 
-from finikie.quickstart.views import UserSerializer, GroupSerializer
+from quickstart.views import UserSerializer, GroupSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.object.all().order_by('-data-joined')
